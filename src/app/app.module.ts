5bc911eb16a0a0ff914a5a058e1d03f4d3a988f8
trilok3003 <triloksingh3003@gmail.com>
2022-05-09 17:21:37 +0530

    3 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { StaticCarouselComponent } from './static-carousel/static-carousel.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { CarouselDirective } from './carousel/carousel.directive';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, StaticCarouselComponent, CarouselComponent, CarouselDirective ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/static-carousel/static-carousel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-static-carousel',
+  templateUrl: './static-carousel.component.html',
+  styleUrls: ['./static-carousel.component.scss']
+})
+export class StaticCarouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
