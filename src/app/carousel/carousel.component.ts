c5c9c02b09d75ab90e5bf63affc7235b7663be3b
trilok3003 <triloksingh3003@gmail.com>
2022-05-09 17:32:36 +0530

    2 files modified, 2 added, 1 deleted

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ContentChildren, OnInit, QueryList } from '@angular/core';
+import { CarouselDirective } from './carousel.directive';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  styleUrls: ['./carousel.component.scss']
 })
 export class CarouselComponent implements OnInit {
-
+  @ContentChildren(CarouselDirective) items : QueryList<CarouselDirective>;
   constructor() { }
 
   ngOnInit() {
